c3e2e094b2c776f2ae0a72110af57ced02a1454e
snamiki1212 <snamiki1212@gmail.com>
2021-03-11 10:07:26 -0800

    Add chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
 import { PATH } from "./constants";
 import { Navigation } from "./components/Navigation";
 import { CurrentPage } from "./components/pages/CurrentPage";
 import { ForecastPage } from "./components/pages/ForecastPage";
 import { HomePage } from "./components/pages/HomePage";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Router>
-      <Navigation />
+    <ChakraProvider>
+      <Router>
+        <Navigation />
 
-      <Switch>
-        <Route path={PATH.current}>
-          <CurrentPage />
-        </Route>
-        <Route path={PATH.forecast}>
-          <ForecastPage />
-        </Route>
-        <Route path={PATH.home}>
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+        <Switch>
+          <Route path={PATH.current}>
+            <CurrentPage />
+          </Route>
+          <Route path={PATH.forecast}>
+            <ForecastPage />
+          </Route>
+          <Route path={PATH.home}>
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
 }
 
