08455dc5a6b7a1e8a6f30ca95be5361f85a68476
snamiki1212 <snamiki1212@gmail.com>
2021-03-20 17:39:08 -0700

    Refactor logic

--- src/components/shared/WeatherItemCard.tsx
+++ src/components/shared/WeatherItemCard.tsx
@@ -3,16 +3,19 @@ import { Icon } from "./Icon";
 import { ForecastWeather, CurrentWeather } from "../../types";
 import { Center, Box, Text, useColorModeValue } from "@chakra-ui/react";
 
-const createDateFromUnixTime = (dt: number) => new Date(dt * 1_000); // REF: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
-
 type Props = { item: ForecastWeather["list"][0] | CurrentWeather };
 
-export function WeatherItemCard({ item }: Props) {
-  const _date = createDateFromUnixTime(item.dt);
-  const day = _date.toDateString();
-  const time = `${String(_date.getHours()).padStart(2, "0")}:${String(
-    _date.getMinutes()
+const createDateFromUnixTime = (dt: number) => new Date(dt * 1_000); // REF: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+
+const renderYYMM = (date: Date) =>
+  `${String(date.getHours()).padStart(2, "0")}:${String(
+    date.getMinutes()
   ).padStart(2, "0")}`;
+
+export function WeatherItemCard({ item }: Props) {
+  const date = createDateFromUnixTime(item.dt);
+  const day = date.toDateString();
+  const time = renderYYMM(date);
 
   return (
     <Center>
